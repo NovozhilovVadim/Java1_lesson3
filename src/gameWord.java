2204b5561304271ce7059e3047737fc7b3a55b05
NovozhilovVadim <novozhilovvadim@gmail.com>
2022-10-10 01:08:32 +0300

    HW game numbers final    game word prerealize

--- src/gameWord.java
+++ src/gameWord.java
@@ -1,16 +1,53 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class gameWord {
     private static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    private static Scanner in = new Scanner(System.in);
+    private static char[] guessword ;
+    private static char[] userWords;
+    private static int size;
 
-
-    public static void main(String[] args) {
-        char[] guessword = arrayWord();
+    public static void startGame() {
+        guessword = arrayWord();
+        size = guessword.length;
+        userWords = emptyfield();
         printChar(guessword);
         System.out.println();
-        emptyfield(guessword.length);
+        printChar(userWords);
+        System.out.println();
+        while (!checkWord(inputUser())){
+            printChar(userWords);
+            System.out.println();
+        }
+        printChar(userWords);
+    }
+
+    private static boolean checkWord(char[] user){
+        if (Arrays.equals(guessword,user)){
+            System.arraycopy(guessword,0, userWords, 0, guessword.length);
+            return true;
+        }else {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < user.length; j++) {
+                    if (guessword[i] == user[j] ){
+                        userWords[i] = user[j];
+                    }
+                }
+            }if (Arrays.equals(guessword,userWords)){
+                return true;
+            }
+            return false;
+        }
+    }
 
 
+
+    private static char[] inputUser(){
+        System.out.println("Please, enter you word: ");
+        char[] userWord = in.next().toCharArray();
+        return userWord;
     }
 
     private static char[] arrayWord(){
@@ -21,12 +58,12 @@ public class gameWord {
         return word;
     }
 // метод запроса слова, методсравнения
-    private static void emptyfield(int size){
+    private static char[] emptyfield(){
         char[] field = new char[size];
         for (int i = 0; i < field.length; i++) {
             field[i] = '#';
         }
-       printChar(field);
+        return field;
     }
     private static void printChar(char[] chars){
         for (int i = 0; i < chars.length; i++) {
